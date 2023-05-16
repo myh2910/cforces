1376cd57ea956b81e2b3a9118c52a78d1bc98a0a
Yohan Min <minleeyohan@gmail.com>
2023-05-16 18:58:07 +0900

    Add templates

---
+++ templates/usaco.cpp
@@ -0,0 +1,15 @@
+/*
+ID: minleey1
+TASK: %s
+LANG: C++
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("%s.in", "r", stdin);
+  freopen("%s.out", "w", stdout);
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  
+  return 0;
+}
